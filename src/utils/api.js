b52f2c5d2e4e07402ac94b94000d88a0a1ae2ddb
Lengskuy <117277264+Lengskuy@users.noreply.github.com>
2025-08-06 15:16:40 +0800

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ||'http://localhost:8000', // Make sure it matches your FastAPI address
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
